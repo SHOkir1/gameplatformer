8f8fdfd90cf71ebc696095275ea1c5a0300da3b4
dmytro.pushkar <s21036@pjwstk.edu.pl>
2023-07-25 17:39:48 +0200

    Q_Start_Update

---
+++ Assets/Scripts/Player_movement/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Horizontal movement
+        float moveInput = Input.GetAxisRaw("Horizontal");
+
+        // Flip the player's sprite based on movement direction
+        if (moveInput < 0f) // Moving left
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        else if (moveInput > 0f) // Moving right
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the player is touching the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
